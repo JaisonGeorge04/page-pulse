138d1b59ca9f44a5eec7ae14c04c819f5501b190
Jaison George <jaisongeorge699@gmail.com>
2026-07-27 00:40:19 +0530

    Change cheerio import to named import syntax

--- src/services/auditService.ts
+++ src/services/auditService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { validateUrlForSsrf } from '../utils/ssrf';
 import { AuditFailedError, ValidationError } from '../utils/errors';
 import { logger } from '../utils/logger';
